7a289849206c95f7f803d5b1fd9d7142fdc1d644
saheedt <stunnafrsh007@gmail.com>
2018-03-01 00:36:55 +0100

    Add filter search bar to listings template

--- template/js/index.js
+++ template/js/index.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', () => {
     const supportsImports =  () => ('import' in document.createElement('link'));
     const landingRegBtn = document.getElementById('landing-reg-btn');
+    const listingsSwitchInput = document.getElementById('listings-switch-input');
+    const listingsCategoryInput = document.querySelector('#listings-category-input');
+    const listingsLocationInput = document.querySelector('#listings-location-input');
 
     //fuction for importing register.html into landing page.
     /** login/register */
@@ -43,6 +46,29 @@ window.addEventListener('load', () => {
         })
     }
 
+    /**listings */
+    if (listingsSwitchInput) {
+        listingsSwitchInput.addEventListener('change', (e)=> {
+            if (e.target.checked) {
+                if (listingsLocationInput.style.display == 'none' ||
+                  listingsLocationInput.style.display == "") {
+                    listingsLocationInput.style.display = 'block'
+                    listingsCategoryInput.style.display = 'none'
+                }
+                return
+            }
+            if (!e.target.checked){
+              if ( listingsCategoryInput.style.display == 'none' ||
+                listingsCategoryInput.style.display == "") {
+                    listingsCategoryInput.style.display = 'block'
+                    listingsLocationInput.style.display = 'none'
+              }
+              return
+            }
+            
+        })
+    }
+    
     /** general */
     window.onclick = (e) => {
         const regModal = document.getElementById('register-modal');
