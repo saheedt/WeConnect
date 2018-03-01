e9bc05ef5c339453a40838487338ae10e1ac5a6e
saheedt <stunnafrsh007@gmail.com>
2018-03-01 22:42:24 +0100

    Implement company profile section in businessfrofile.html

--- template/dummy/db.js
+++ template/dummy/db.js
@@ -4,26 +4,31 @@ window.DB = [
         address: 'Plot 759, Central Business District',
         state: 'Abuja',
         description: 'Delivering excellent legal services.',
+        employees: 22,
         phonenumber: 122346776
     },
     {
         businessName: 'm and j',
-        address: 'Plot 59, lagos island',
-        state:'lagos',
+        address: 'Plot 59, y island',
+        state:'Oyo',
         description: 'discount like never before.',
+        employees: 2,
         phonenumber: 122346776
     },
     {
         businessName: 'x venture',
         address: '19, xyz street',
-        state: 'kano',
+        state: 'Kano',
         description: 'we sell games.',
+        employees: 1,
         phonenumber: 122346776
     },
     {
         businessName: 'Andela',
         address: 'Epic towers, ilupeju, alagbado',
-        description: 'Paying you to learn to code.',
+        state: 'Lagos',
+        description: 'Paying you to learn.',
+        employees: 450,
         phonenumber: 122346776
     }
 ];
