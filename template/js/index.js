d21e9e668a181f71f34474e787a0f77d94eebbe1
saheedt <stunnafrsh007@gmail.com>
2018-03-02 10:32:31 +0100

    Remove html import as it doesn't play nice with chrome and safari without web server + delete register.html since no import required.

--- template/js/index.js
+++ template/js/index.js
@@ -8,44 +8,18 @@ window.addEventListener('load', () => {
 
     const listingsList = document.querySelector('#listings-list');
 
-    //fuction for importing register.html into landing page.
     /** login/register */
-    const includeRegister = () => {
-        const element = document.getElementById('registerInclude');
-        const file = element.getAttribute("w3-include-html");
-        let xhttp;
-        if (file) {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-              if (this.readyState == 4) {
-                if (this.status == 200) {element.innerHTML = this.responseText;}
-                if (this.status == 404) {element.innerHTML = "Page not found.";}
-                element.removeAttribute("w3-include-html");
-                includeRegister();
-              }
-            }      
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            return;
-          }
-    }
     if (landingRegBtn) {
         landingRegBtn.addEventListener('click', (e) => {
             e.preventDefault();
-            // if (supportsImports()) {
-            //     const link = document.querySelector('link[rel="import"]'),
-            //         imp = link.import,
-            //         element = imp.querySelector('.generic-modal');
-            //     document.body.appendChild(element.cloneNode(true));
-            // }
             const regModal = document.getElementById('register-modal')
-            if ( regModal ){
-                if ( regModal.style.display == "none" ){
-                    regModal.style.display = "block";
+            if ( regModal ) {
+                if ( regModal.style.display == "block" ) {
+                    regModal.style.display = "none";
+                } else {
+                    regModal.style.display = "block"
                 }
-                return;
             }
-            includeRegister();
         })
     }
 
