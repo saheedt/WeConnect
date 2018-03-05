3ab57b3879c18da14dcde69fb947c14383d8ac4b
saheedt <stunnafrsh007@gmail.com>
2018-03-05 08:40:39 +0100

    [chore] add .travis.yml file to describe  travic ci configuration

--- template/js/index.js
+++ template/js/index.js
@@ -1,90 +1,83 @@
+// const { window } = this.window;
+this.window.addEventListener('load', () => {
+  const landingRegBtn = this.window.document.getElementById('landing-reg-btn');
+  const signupBtn = this.window.document.getElementById('signup-btn');
+  const listingsSwitchInput = this.window.document.getElementById('listings-switch-input');
+  const listingsCategoryInput = this.window.document.querySelector('#listings-category-input');
+  const listingsLocationInput = this.window.document.querySelector('#listings-location-input');
+  const addBusinessBtn = this.window.document.getElementById('add-business-btn');
+  const updateBusinessBtn = this.window.document.getElementById('update-business-btn');
+  const postReview = this.window.document.getElementById('post-review');
 
-window.addEventListener('load', () => {
-    const supportsImports =  () => ('import' in document.createElement('link'));
-    const landingRegBtn = document.getElementById('landing-reg-btn');
-    const signupBtn = document.getElementById('signup-btn');
-    const listingsSwitchInput = document.getElementById('listings-switch-input');
-    const listingsCategoryInput = document.querySelector('#listings-category-input');
-    const listingsLocationInput = document.querySelector('#listings-location-input');
-    const addBusinessBtn = document.getElementById('add-business-btn');
-    const updateBusinessBtn = document.getElementById('update-business-btn');
-    const postReview = document.getElementById('post-review');
+  const listingsList = this.window.document.querySelector('#listings-list');
 
-    const listingsList = document.querySelector('#listings-list');
-
-    /** login/signup */
-    if (landingRegBtn) {
-        landingRegBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            const regModal = document.getElementById('register-modal')
-            if ( regModal ) {
-                if ( regModal.style.display == "block" ) {
-                    regModal.style.display = "none";
-                } else {
-                    regModal.style.display = "block"
-                }
-            }
-        })
-    }
-    if (signupBtn) {
-        signupBtn.addEventListener('click', (e) => {
-            const regModal = document.getElementById('register-modal')
-            e.preventDefault();
-            regModal.style.display = "none";
-            alert('Sign up Successful');
-            
-        });
-    }
-    
-
-    /** Post review */
-    if (postReview) {
-        postReview.addEventListener('click', (e) => {
-            alert('Review added.')
-        })
-    }
-    /**Update business */
-    if (updateBusinessBtn) {
-        updateBusinessBtn.addEventListener('click', (e) => {
-            alert('Business profile successfully update.')
-        });
-    }
-    /**Register business */
-    if (addBusinessBtn) {
-        addBusinessBtn.addEventListener('click', (e) => {
-            alert('Business successfully registered.')
-        });
-    }
-    /**listings */
-    if (listingsSwitchInput) {
-        listingsSwitchInput.addEventListener('change', (e)=> {
-            if (e.target.checked) {
-                if (listingsLocationInput.style.display == 'none' ||
-                  listingsLocationInput.style.display == "") {
-                    listingsLocationInput.style.display = 'block'
-                    listingsCategoryInput.style.display = 'none'
-                }
-                return
-            }
-            if (!e.target.checked){
-              if ( listingsCategoryInput.style.display == 'none' ||
-                listingsCategoryInput.style.display == "") {
-                    listingsCategoryInput.style.display = 'block'
-                    listingsLocationInput.style.display = 'none'
-              }
-              return
-            }
-            
-        })
-    }
-
-    if ( listingsList ) {
-        console.log(DB);
-        let list = [];
-        for ( let biz of DB) {
-            let li = document.createElement("li");
-            li.className = "collection-item"
-            li.innerHTML = `
+  /** login/signup */
+  if (landingRegBtn) {
+    landingRegBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const regModal = this.window.document.getElementById('register-modal');
+      if (regModal) {
+        if (regModal.style.display === 'block') {
+          regModal.style.display = 'none';
+        } else {
+          regModal.style.display = 'block';
+        }
+      }
+    });
+  }
+  if (signupBtn) {
+    signupBtn.addEventListener('click', (e) => {
+      const regModal = this.window.document.getElementById('register-modal')
+      e.preventDefault();
+      regModal.style.display = 'none';
+      alert('Sign up Successful');     
+    });
+  }
+  /** Post review */
+  if (postReview) {
+    postReview.addEventListener('click', () => {
+      alert('Review added.')
+    });
+  }
+  /** Update business */
+  if (updateBusinessBtn) {
+    updateBusinessBtn.addEventListener('click', () => {
+      alert('Business profile successfully update.')
+    });
+  }
+  /** Register business */
+  if (addBusinessBtn) {
+    addBusinessBtn.addEventListener('click', () => {
+      alert('Business successfully registered.')
+    });
+  }
+  /** listings */
+  if (listingsSwitchInput) {
+    listingsSwitchInput.addEventListener('change', (e) => {
+      if (e.target.checked) {
+        if (listingsLocationInput.style.display === 'none' ||
+          listingsLocationInput.style.display === '') {
+          listingsLocationInput.style.display = 'block';
+          listingsCategoryInput.style.display = 'none';
+        }
+        return;
+      }
+      if (!e.target.checked) {
+        if (listingsCategoryInput.style.display === 'none' ||
+          listingsCategoryInput.style.display === '') {
+          listingsCategoryInput.style.display = 'block';
+          listingsLocationInput.style.display = 'none';
+        }
+      }
+    });
+  }
+  if (listingsList) {
+    const list = [];
+    // for (let biz of DB) {
+    this.window.DB.map((biz) => {
+      const li = this.window.document.createElement('li');
+      li.className = 'collection-item';
+      li.innerHTML = `
         <!-- <li class="collection-item avatar"> -->
         <div class="listings-list-groupings flex" >
         <div class="listings-list-groupings-items-left flex"> <i class="material-icons circle">business_center</i></div>
@@ -100,18 +93,16 @@ window.addEventListener('load', () => {
         <div class="listings-list-groupings-items-left flex"><i class="material-icons circle">description</i></div>
         <div class="listings-list-groupings-items-right"><p>${biz.description}</p></div>
         </div>`;
-        list.push(li);
-        }
-        for ( let ele of list) {
-        listingsList.appendChild(ele);
-        }
+      return list.push(li);
+    });
+    // for ( let ele of list) {
+    list.map(ele => listingsList.appendChild(ele));
+  }
+
+  this.window.onclick = (e) => {
+    const regModal = this.window.document.getElementById('register-modal');
+    if (e.target === regModal) {
+      regModal.style.display = 'none';
     }
-    
-    /** general */
-    window.onclick = (e) => {
-        const regModal = document.getElementById('register-modal');
-        if (e.target === regModal ) {
-            regModal.style.display = "none";
-        }
-    }
+  };
 });
