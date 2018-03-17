e3e0318daf7e4775f21805de8a5c24090b0d9101
saheedt <stunnafrsh007@gmail.com>
2018-03-17 22:41:52 +0100

    feature(request-password-reset): Remove log statements

--- server/tests/test.spec.js
+++ server/tests/test.spec.js
@@ -264,7 +264,6 @@ describe('user endpoints', () => {
           })
           .expect('Content-Type', /json/)
           .end((err, resp) => {
-            console.log(resp.body);
             assert.deepEqual(resp.status, 200);
             assert.deepEqual(
               resp.body.message,
