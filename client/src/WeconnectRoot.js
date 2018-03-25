0f20c891d2349e856b012b4f8520e7f0f27578d5
saheedt <stunnafrsh007@gmail.com>
2018-03-25 18:30:51 +0100

    feature(react-redux): add styles.scss and app entry (<WeconnectRoot />) component

--- client/src/WeconnectRoot.js
+++ client/src/WeconnectRoot.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-// import './css/styles.css';
+import './css/styles.scss';
 
 render(
   <Provider>
