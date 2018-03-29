4719a920c59739a1162807a2a359ce10456d5836
saheedt <stunnafrsh007@gmail.com>
2018-03-29 02:49:58 +0100

    feature(react-redux): add <Loader />

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
 
 class Loader extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      businesses: null
-    };
-  }
-  componentWillMount() {
-  }
-  componentWillReceiveProps() {
-  }
   render() {
     return (
       <div className="loader-holder">
@@ -21,11 +9,4 @@ class Loader extends Component {
     );
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-    ...state.businesses
-  };
-};
-
-export default connect(mapStateToProps)(Loader);
+export default Loader;
